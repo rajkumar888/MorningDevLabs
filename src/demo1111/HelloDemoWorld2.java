d68e8993f842e6f2ebc46b3d21a99f063c90d556
rajkumar888 <Soma@rajeev67>
2022-01-08 23:57:39 +0800

    demo purpose commit from trainer

---
+++ src/demo1111/HelloDemoWorld2.java
@@ -0,0 +1,13 @@
+package demo1111;
+
+public class HelloDemoWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println(" Hello world demo example");
+
+	}
+
+}
